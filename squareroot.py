2c69beb78894d9b267a017eadc0522f9e2b85049
yace99 <46865551+yace99@users.noreply.github.com>
2019-01-20 15:46:30 +0100

    Update squareroot.py

--- squareroot.py
+++ squareroot.py
@@ -1,24 +1,22 @@
-#extraction de racine carrée par dichotomie
+#extraction de racine carrÃ©e par dichotomie
 #par Yassine Sahraoui
 
 def dichosqrt(number):
     
     if number < 0:
-        return 0 #dans le cas ou l'utilisateur entre un nombre négatif, le programme retournera 0
+        return 0 #dans le cas ou l'utilisateur entre un nombre nÃ©gatif, le programme retournera 0
     else:
-        mini = 0.0 #borne inferieure de départ: 0
-        maxi = round(number, 4) #borne supérieure de départ: le nombre entré par l'utilisateur
+        mini = 0.0 #borne inferieure de dÃ©part: 0
+        maxi = round(number, 4) #borne supÃ©rieure de dÃ©part: le nombre entrÃ© par l'utilisateur
         mid = round(mini+maxi/2.0, 4) #moyenne de la borne sup et inf
         
         while True:
-            mid = round((mini+maxi)/2.0, 5) #reévalution de "mid" a chaque iteration 
-            diff = mid*mid - round(number, 4) #variable marge de précision
+            mid = round((mini+maxi)/2.0, 5) #reÃ©valution de "mid" a chaque iteration 
+            diff = mid*mid - round(number, 4) #variable marge de prÃ©cision
             if abs(diff) <= 0.001:
-                return round(mid, 4)  # si la précision atteint 0.001, la boucle s'arrête et le programme renvoie le resultat
+                return round(mid, 4)  # si la prÃ©cision atteint 0.001, la boucle s'arrÃªte et le programme renvoie le resultat
             elif mid*mid < number: 
-                mini = mid # si le carré de "mid" est inferrieur au nombre, on reduit la zone de recherche à  la moitié supérieur
-                print("1", mid)
+                mini = mid # si le carrÃ© de "mid" est inferrieur au nombre, on reduit la zone de recherche Ã   la moitiÃ© supÃ©rieur
             else:
-                maxi = mid # si le carré de "mid" est superieur au nombre, on reduit la zone de recherche à la moitié inférieure
-                print("2", mid)
+                maxi = mid # si le carrÃ© de "mid" est superieur au nombre, on reduit la zone de recherche Ã  la moitiÃ© infÃ©rieure
 
